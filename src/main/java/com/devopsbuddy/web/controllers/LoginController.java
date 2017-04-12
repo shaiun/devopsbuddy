3124759c75e90a69acb1958eea3f93d680cd3d4f
shaiun <shaiun@outlook.com>
2017-04-13 11:43:49 +1200

    Added login form and login link to the navbar

---
+++ src/main/java/com/devopsbuddy/web/controllers/LoginController.java
@@ -0,0 +1,19 @@
+package com.devopsbuddy.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by shaiun on 13/04/17.
+ */
+@Controller
+public class LoginController {
+
+    /** The login view name */
+    public static final String LOGIN_VIEW_NAME = "user/login";
+
+    @RequestMapping("/login")
+    public String login() {
+        return LOGIN_VIEW_NAME;
+    }
+}
